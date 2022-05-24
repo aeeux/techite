936146f1d1122207a123bac7f9c2d398b2129310
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-05-24 17:25:04 +0200

    Create about.tsx

---
+++ site/pages/about.tsx
@@ -0,0 +1,13 @@
+import Hero from '@components/about/Hero'
+import AboutUs from '@components/about/AboutUs'
+import History from '@components/about/History'
+
+export default function About () {
+  return (
+    <div>
+      <Hero />
+      <History />
+      <AboutUs />
+      </div>
+  )
+}
