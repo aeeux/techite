12df6b3a693ee46091fbf820b35407e716e6dd95
lucasjolibois54 <lucasjolibois@icloud.com>
2022-05-24 17:41:04 +0200

    Update about.tsx

--- site/pages/about.tsx
+++ site/pages/about.tsx
@@ -1,6 +1,7 @@
 import Hero from '@components/about/Hero'
 import AboutUs from '@components/about/AboutUs'
 import History from '@components/about/History'
+import Newsletter from '@components/about/Newsletter'
 
 export default function About () {
   return (
@@ -8,6 +9,7 @@ export default function About () {
       <Hero />
       <History />
       <AboutUs />
+      <Newsletter/>
       </div>
   )
 }
