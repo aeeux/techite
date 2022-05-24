4c5127d5989f2753d2aad6c8275d1631677517a4
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-05-24 18:04:34 +0200

    Update ProductView.tsx

--- site/components/product/ProductView/ProductView.tsx
+++ site/components/product/ProductView/ProductView.tsx
@@ -40,9 +40,9 @@ const ProductView: FC<ProductViewProps> = ({ product, relatedProducts }) => {
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M9 5l7 7-7 7"
                 />
               </svg>
@@ -59,9 +59,9 @@ const ProductView: FC<ProductViewProps> = ({ product, relatedProducts }) => {
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M9 5l7 7-7 7"
                 />
               </svg>
