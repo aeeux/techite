20658771352378d94d85f83fe54c41786b9b55a5
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-05-24 17:36:50 +0200

    Update about.tsx

--- site/pages/about.tsx
+++ site/pages/about.tsx
@@ -1,6 +1,7 @@
 import Hero from '@components/about/Hero'
 import AboutUs from '@components/about/AboutUs'
 import History from '@components/about/History'
+import Footer from '@components/common/Footer/Footer'
 
 export default function About () {
   return (
@@ -8,6 +9,7 @@ export default function About () {
       <Hero />
       <History />
       <AboutUs />
+      <Footer />
       </div>
   )
 }
