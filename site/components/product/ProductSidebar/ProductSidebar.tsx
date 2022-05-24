2b686e9d7de0ffe23a86ebdff4d5fe7a4e909263
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-05-24 21:02:46 +0200

    Update ProductSidebar.tsx

--- site/components/product/ProductSidebar/ProductSidebar.tsx
+++ site/components/product/ProductSidebar/ProductSidebar.tsx
@@ -63,40 +63,6 @@ const ProductSidebar: FC<ProductSidebarProps> = ({ product, className }) => {
     </div>
   </div>
 </div>
-<div className="md:hidden">
-  <div className="group inline-block text-black mb-10">
-    <button className="outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32">
-      <span className="pr-1 font-semibold flex-1">Komponenter</span>
-      <span>
-        <svg
-          className="fill-current h-4 w-4 transform group-hover:-rotate-180
-transition duration-150 ease-in-out"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-        </svg>
-      </span>
-    </button>
-    <ul
-      className="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute 
-transition duration-150 ease-in-out origin-top min-w-32"
-    >
-      <li className="rounded-sm px-3 py-1 hover:bg-gray-100">
-        Zotac Gaming Gerforce RTX 3080 Ti Trinity OC
-      </li>
-      <li className="rounded-sm px-3 py-1 hover:bg-gray-100">
-        32 GB Kingston Fury Beast DDR5 RAM (2x16 GB)
-      </li>
-      <li className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
-        Intel i9 12.gen - 16 kerner 3.2 Ghz (5.2 Turbo) Processor{' '}
-      </li>
-      <li className="rounded-sm px-3 py-1 hover:bg-gray-100">
-        Kingston 2 TB NVMe M.2 SSD
-      </li>
-    </ul>
-  </div>
-</div>
       <div>
         {process.env.COMMERCE_CART_ENABLED && (
           <Button
