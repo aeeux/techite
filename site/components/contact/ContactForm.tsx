d9c7d13c15d9870e43f9717c60adba95094394ed
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-05-25 18:55:18 +0200

    Update ContactForm.tsx

--- site/components/contact/ContactForm.tsx
+++ site/components/contact/ContactForm.tsx
@@ -2,7 +2,7 @@ function ContactForm() {
   return (
     <div className=" md:flex md:mx-0">
       <div className="w-1/3 md:w-2/h-24 md:h-screen bg-white md:bg-black py-12 px-6 space-y-12">
-        <h2 className="text-black md:text-white text-2xl font-semibold mt-12 md:mx-5">
+        <h2 className="text-black md:text-white md:text-3xl text-2xl font-semibold mt-12 md:mx-5">
           Kontakt informationer
         </h2>
         <p className="text-black md:text-white font-semibold md:mx-5">
@@ -28,7 +28,7 @@ function ContactForm() {
             <input
               type="name"
               placeholder="fornavn"
-              className="placeholder:italic placeholder:text-slate-400 border-4 border-emerald-300"
+              className="placeholder:italic placeholder:text-slate-400 border border-gray-400 rounded-md w-full mt-4 py-1"
             />
           </label>
           <label className="block">
@@ -38,7 +38,7 @@ function ContactForm() {
             <input
               type="lastname"
               placeholder="efternavn"
-              className="placeholder:italic placeholder:text-slate-400 border-4 border-emerald-300"
+              className="placeholder:italic placeholder:text-slate-400 border border-gray-400 rounded-md w-full mt-4 py-1"
             />
           </label>
           <label className="block">
@@ -47,8 +47,8 @@ function ContactForm() {
             </span>
             <input
               type="email"
-              placeholder="email"
-              className="placeholder:italic placeholder:text-slate-400 border-4 border-emerald-300"
+              placeholder="eksempel@email.dk"
+              className="placeholder:italic placeholder:text-slate-400 border border-gray-400 rounded-md w-full mt-4 py-1"
             />
           </label>
           <label className="block">
@@ -58,21 +58,21 @@ function ContactForm() {
             <input
               type="phone"
               placeholder="telefon"
-              className="placeholder:italic placeholder:text-slate-400 border-4 border-emerald-300"
+              className="placeholder:italic placeholder:text-slate-400 border border-gray-400 rounded-md w-full mt-4 py-1"
             />
           </label>
+        </div>
           <label className="block">
-            <span className="block text-sm font-medium text-slate-700">
+            <span className="block text-sm font-medium text-slate-700 mt-10">
               Message
             </span>
             <input
               type="message"
-              placeholder="message"
-              className="placeholder:italic placeholder:text-slate-400 border-4 border-emerald-300"
+              placeholder="Skriv en besked til os her"
+              className="placeholder:italic placeholder:text-slate-400 border-b border-black w-full mt-4 py-1 mb-10"
             />
           </label>
-        </div>
-        <button className="border-4 border-emerald-300 float-right">
+        <button className="float-right font-semibold rounded-md bg-button-b hover:bg-blue-600 text-white mt-4 text-sm py-2 px-10">
           Send Formular
         </button>
       </div>
