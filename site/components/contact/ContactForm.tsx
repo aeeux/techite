dc563333551c5983cc6693bb1d7635be281105b3
AeeUX <AndreasEriksen99@hotmail.com>
2022-05-25 17:35:13 +0200

    Update ContactForm.tsx

--- site/components/contact/ContactForm.tsx
+++ site/components/contact/ContactForm.tsx
@@ -1,113 +1,80 @@
-import Link from 'next/link'
-import styles from './contact.module.css'
-
 function ContactForm() {
   return (
-    <div className={styles.Ccontent}>
-      <div className="max-w-7xl mx-auto p-5">
-        <div className="grid grid-cols-1 md:grid-cols-12 border">
-          <div className="bg-gray-900 md:col-span-4 p-10 text-white">
-            <p className="mt-4 text-sm leading-7 font-regular uppercase">
-              Kontakt os
-            </p>
-            <h3 className="text-3xl leading-normal font-extrabold tracking-tight">
-              Kontakt Informationer
-            </h3>
-            <p className="mt-4 leading-7 text-gray-200">
-              Udfyld formularen, og vores team vender tilbage til dig indenfor
-              24 timer
-            </p>
-
-            <div className="flex items-center mt-5">
-              <span className="text-sm">+45 12 34 56 78</span>
-            </div>
-            <div className="flex items-center mt-5">
-              <span className="text-sm">hello@gmail.com</span>
-            </div>
-            <div className="flex items-center mt-5">
-              <span className="text-sm">Fjeldhammervej 15</span>
-            </div>
-          </div>
-          <form className="md:col-span-8 p-10">
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Fornavn
-                </label>
-                <input
-                  className="appearance-none bg-transparent border-b border-indigo-500 w-full text-gray-700 mr-3 p-4 leading-tight focus:outline-none focus:bg-gray-200"
-                  type="text"
-                  placeholder="Fornavn"
-                  aria-label="Full name"
-                />
-              </div>
-              <div className="w-full md:w-1/2 px-3">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Efternavn
-                </label>
-                <input
-                  className="appearance-none bg-transparent border-b border-indigo-500 w-full text-gray-700 mr-3 p-4 leading-tight focus:outline-none focus:bg-gray-200"
-                  type="text"
-                  placeholder="Efternavn"
-                  aria-label="Full name"
-                />
-              </div>
-            </div>
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  E-mail
-                </label>
-                <input
-                  className="appearance-none bg-transparent border-b border-indigo-500 w-full text-gray-700 mr-3 p-4 leading-tight focus:outline-none focus:bg-gray-200"
-                  type="text"
-                  placeholder="eksempel@email.dk"
-                  aria-label="Full name"
-                />
-              </div>
-              <div className="w-full md:w-1/2 px-3">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Telefon
-                </label>
-                <input
-                  className="appearance-none bg-transparent border-b border-indigo-500 w-full text-gray-700 mr-3 p-4 leading-tight focus:outline-none focus:bg-gray-200"
-                  type="text"
-                  placeholder="+45 12 34 56 78"
-                  aria-label="Full name"
-                />
-              </div>
-            </div>
-
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Message
-                </label>
-                <textarea
-                  className="appearance-none bg-transparent border-b border-indigo-500 w-full text-gray-700 mr-3 p-4 focus:bg-gray-200 leading-tight focus:outline-none"
-                  placeholder="Skriv en besked til os her"
-                  aria-label="Full name"
-                ></textarea>
-              </div>
-              <div className="flex justify-between w-full px-3">
-                <div className="md:flex md:items-center">
-                  <label className="block text-gray-500 font-bold">
-                    <input className="mr-2 leading-tight" type="checkbox" />
-                    <span className="text-sm">
-                      Tilmeld dig vores nyhedsbrev!
-                    </span>
-                  </label>
-                </div>
-                <button
-                  className="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
-                  type="submit"
-                >
-                  Send formular
-                </button>
-              </div>
-            </div>
-          </form>
+    <div className=" md:flex md:mx-0">
+      <div className="w-1/3 md:w-2/h-24 md:h-screen bg-white md:bg-black py-12 px-6 space-y-12">
+        <h2 className="text-black md:text-white text-2xl font-semibold mt-12 md:mx-5">
+          Kontakt informationer
+        </h2>
+        <p className="text-black md:text-white font-semibold md:mx-5">
+          Udfyld formularen, og vores team vender tilbage til dig indenfor 24
+          timer
+        </p>
+        <p className="text-black md:text-white font-semibold md:mx-5">
+          +45 12 34 56 78
+        </p>
+        <p className="text-black md:text-white font-semibold md:mx-5">
+          hello@gmail.com
+        </p>
+        <p className="text-black md:text-white font-semibold md:mx-5">
+          Fjeldhammervej 15{' '}
+        </p>
+      </div>
+      <div className="py-12 px-6 md:w-1/2 h-full mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-12">
+          <label className="block">
+            <span className="block text-sm font-medium text-slate-700">
+              Fornavn
+            </span>
+            <input
+              type="name"
+              placeholder="fornavn"
+              className="placeholder:italic placeholder:text-slate-400 border-4 border-emerald-300"
+            />
+          </label>
+          <label className="block">
+            <span className="block text-sm font-medium text-slate-700">
+              Efternavn
+            </span>
+            <input
+              type="lastname"
+              placeholder="efternavn"
+              className="placeholder:italic placeholder:text-slate-400 border-4 border-emerald-300"
+            />
+          </label>
+          <label className="block">
+            <span className="block text-sm font-medium text-slate-700">
+              Email
+            </span>
+            <input
+              type="email"
+              placeholder="email"
+              className="placeholder:italic placeholder:text-slate-400 border-4 border-emerald-300"
+            />
+          </label>
+          <label className="block">
+            <span className="block text-sm font-medium text-slate-700">
+              Telefon
+            </span>
+            <input
+              type="phone"
+              placeholder="telefon"
+              className="placeholder:italic placeholder:text-slate-400 border-4 border-emerald-300"
+            />
+          </label>
+          <label className="block">
+            <span className="block text-sm font-medium text-slate-700">
+              Message
+            </span>
+            <input
+              type="message"
+              placeholder="message"
+              className="placeholder:italic placeholder:text-slate-400 border-4 border-emerald-300"
+            />
+          </label>
         </div>
+        <button className="border-4 border-emerald-300 float-right">
+          Send Formular
+        </button>
       </div>
     </div>
   )
