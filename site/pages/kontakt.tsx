0c2ba907ea4278bd0d802422aec30678ec1a1416
AeeUX <AndreasEriksen99@hotmail.com>
2022-05-25 17:35:14 +0200

    Update kontakt.tsx

--- site/pages/kontakt.tsx
+++ site/pages/kontakt.tsx
@@ -1,9 +1,12 @@
 import ContactForm from '@components/contact/ContactForm'
+import { Layout } from '@components/common'
 
-export default function About() {
+export default function Contact() {
   return (
     <div>
       <ContactForm />
     </div>
   )
 }
+
+Contact.Layout = Layout
