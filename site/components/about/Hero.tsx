6169fb5b5ea165a4ba0cdb393e1a6eb14b5e0d65
lucasjolibois54 <lucasjolibois@icloud.com>
2022-05-27 18:20:48 +0200

    Update Hero.tsx

--- site/components/about/Hero.tsx
+++ site/components/about/Hero.tsx
@@ -5,7 +5,7 @@ function Hero() {
     <div>
       <div className='lg:max-w-7xl mx-auto'>
         <h1 className="text-3xl md:text-7xl lg:text-8xl text-center mt-52 mb-10 font-semibold">Computer Specialists</h1>
-        <h2 className="text-xl text-center mb-20">Hos Techite Gaming for du kraftfulde gaming computere til netop dit behov!</h2>
+        <h2 className="text-xl text-center mb-20">Hos Techite Gaming får du kraftfulde gaming computere til netop dit behov!</h2>
         <div className='center'>
         <img
               className="w-11/12 mx-auto"
