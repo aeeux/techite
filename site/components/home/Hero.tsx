2887daf292cb43bf686984cfa0c7a1d9529f0a2b
AeeUX <AndreasEriksen99@hotmail.com>
2022-06-02 15:49:53 +0200

    Update Hero.tsx

--- site/components/home/Hero.tsx
+++ site/components/home/Hero.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link'
 function Hero() {
   return (
     <div>
-        <h1 className='text-center my-20 pt-20 pb-10 text-4xl md:text-6xl font-bold'>
-        Gaming & PC <br/>made simple.
-        </h1>
+      <h1 className="text-center my-20 pt-20 pb-10 text-4xl md:text-6xl font-bold">
+        Gaming & PC <br />
+        made simple.
+      </h1>
     </div>
-
-  );
+  )
 }
 
-export default Hero;
+export default Hero
